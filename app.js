1359be0ab32be0d540d0d25c35d5522649bb3f96
James Dineley <jdineley@hotmail.com>
2022-09-02 23:13:00 +0100

    add full functionality

--- app.js
+++ app.js
@@ -1,47 +1,95 @@
+// DOM node capture
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
+const numSquaresBut = document.querySelector("#num-squares");
+const rainbowBut = document.querySelector("#rainbow");
+const singleColorBut = document.querySelector("#singleColor");
+const resetBoardBut = document.querySelector("#reset")
+const shadeBut = document.querySelector("#shade");
 
+// Global inputs
+let rainbowOn = false;
+let shadeOn = false
+let color = 'green'
+let size = 4
 
-for (let i = 1; i <= 16; i++){
-    let div = document.createElement('div');
-    div.classList.add("cell");
-    container.append(div);
-}
-
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', (e) => {
-        e.target.classList.add("hover");
-    })
-})
-
-function buildBoard(size){
+// Event handlers
+function resetBoard(e) {
     container.innerHTML = ''
     for (let i = 1; i <= size*size; i++){
         let div = document.createElement('div');
-        div.classList.add("new-cell");
+        div.classList.add("cell");
+        div.setAttribute('data-opacity', '0')
         let width = 100/size;
         div.style.cssText = `width: ${width}%; aspect-ratio: 1/1; border: black 1px solid`
         container.append(div);
     }
-    const cells = document.querySelectorAll(".new-cell");
+    const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
-        cell.addEventListener('mouseover', (e) => {
-            e.target.classList.add("hover");
-        })
+        cell.addEventListener('mouseover', colorHandler)
     })
 }
 
-function changeBoardSize() {
-    let size = prompt('select the number of squares less than 60');
-    buildBoard(size);
-
+function changeBoardSize(e) {
+    size = prompt('select the number of squares less than 60');
+    resetBoard()
 };
 
-button.addEventListener('click', changeBoardSize);
+function rainbowColors(e){
+    if (!rainbowOn) rainbowOn = true;
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function choseColor(e) {
+    rainbowOn = false;
+    shadeOn = false;
+    color = prompt('Choose a color');
+}
+
+function shade(e){
+    rainbowOn = false;
+    if(!shadeOn) {
+        shadeOn = true; 
+    }
+    console.dir(e.target.getAttribute('class') === 'cell');
+    if(e.target.getAttribute('class') === 'cell'){   
+        console.dir(e.target.getAttribute('data-opacity'));
+        let opacity = Number(e.target.getAttribute('data-opacity'));
+        e.target.setAttribute('data-opacity', `${opacity+0.1}`)
+        opacity = Number(e.target.getAttribute('data-opacity'))
+        return `rgba(0,0,0,${opacity})`
+    }
+
+
+}
+
+function colorHandler(e) {
+        if(rainbowOn){
+            e.target.style.backgroundColor = rainbowColors(e)
+        } else if(shadeOn){
+            e.target.style.backgroundColor = shade(e);
+        } 
+        else {
+            e.target.style.backgroundColor = color;
+        }
+    }
+
+
+// Intial load setup
+
+resetBoard();
 
 
 
+// Event listeners
+numSquaresBut.addEventListener('click', changeBoardSize);
 
+rainbowBut.addEventListener('click', rainbowColors);
 
+singleColorBut.addEventListener('click', choseColor);
+
+resetBoardBut.addEventListener('click', resetBoard);
+
+shadeBut.addEventListener('click', shade);
